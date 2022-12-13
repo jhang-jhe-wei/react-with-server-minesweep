9a5bfda79b085c4134a520fd6be52a190fd8b101
wells <jhang0912407249@gmail.com>
2022-12-13 22:54:39 +0100

    Refactor: Extract head to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,106 @@
 import React from 'react';
 import './App.css';
+import Head from './Head';
 
 const App = () => {
   return (
-  <div className="container">
-    <div className="title-field">
-      <div>
-        <h1 className="title">Minesweeper</h1>
-        <h2 className="subtitle">by JHE-WEI, JHANG</h2>
+    <div className="container">
+      <Head/>
+      <div className="play-field">
+        <div className="cell cell-checked"></div>
+        <div className="cell cell-checked symbol-1"></div>
+        <div className="cell cell-checked symbol-bomb"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell cell-checked symbol-1"></div>
+        <div className="cell cell-checked symbol-2"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell symbol-flag"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="buttons-field">
+        <button>Small</button>
+        <button>Medium</button>
+        <button>Large</button>
+      </div>
+      <div className="footer-field">
+        <p className="footer">
+          ©2022 by JHE-WEI, JHANG
+        </p>
       </div>
     </div>
-    <div className="play-field">
-      <div className="cell cell-checked"></div>
-      <div className="cell cell-checked symbol-1"></div>
-      <div className="cell cell-checked symbol-bomb"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell cell-checked symbol-1"></div>
-      <div className="cell cell-checked symbol-2"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell symbol-flag"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-      <div className="cell"></div>
-    </div>
-    <div className="buttons-field">
-      <button>Small</button>
-      <button>Medium</button>
-      <button>Large</button>
-    </div>
-    <div className="footer-field">
-      <p className="footer">
-        ©2022 by JHE-WEI, JHANG
-      </p>
-    </div>
-  </div>
-);
+  );
 }
 
 export default App;
---
+++ src/Head.tsx
@@ -0,0 +1,12 @@
+const Head = () => {
+  return (
+    <div className="title-field">
+      <div>
+        <h1 className="title">Minesweeper</h1>
+        <h2 className="subtitle">by JHE-WEI, JHANG</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Head;
