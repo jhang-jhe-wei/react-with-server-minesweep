266d6fb6b5d86a88341cab737c7f97e0fc295af3
wells <jhang0912407249@gmail.com>
2022-12-13 23:20:08 +0100

    Refactor: Extract footer to component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,15 @@ import './App.css';
 import Head from './Head';
 import PlayField from './PlayField';
 import ButtonField from './ButtonField';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <div className="container">
       <Head/>
-      <div className="footer-field">
-        <p className="footer">
-          ©2022 by JHE-WEI, JHANG
-        </p>
-      </div>
       <PlayField/>
       <ButtonField/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer-field">
+      <p className="footer">
+        ©2022 by JHE-WEI, JHANG
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
